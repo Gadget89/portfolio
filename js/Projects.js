6d02c87ebbf75195d9cc643f16f119981adb232f
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-29 09:39:42 -0600

    Changed layout of nav buttons

--- js/Projects.js
+++ js/Projects.js
@@ -5,12 +5,12 @@ export default React.createClass({
   render() {
     return (
       <section className="center_wrapper">
-        <h1>Projects</h1>
+        <h1 className="page_title">Projects</h1>
         <span className="nav_button"><Link to="/">Home</Link></span>
         <p>
           I had the oppertunity to contribute to a group project halfway through the cohort. The project used Trello for project managment and we had four members on our team. It was challenging and turned out to be a great expierience. Follow the link below to check out this project on GitHub.
         </p>
-        <a target="_blank" href="https://github.com/luciogutz/client-week-project">Click here</a>
+        <a target="_blank" href="https://client-week-project.herokuapp.com/">Click here</a>
       </section>
     )
   }
--- js/Welcome.js
+++ js/Welcome.js
@@ -5,11 +5,11 @@ export default React.createClass({
   render() {
     return(
       <section className="center_wrapper">
-        <h1>Welcome!</h1>
-        <div>          
+        <h1 className="page_title">Welcome!</h1>
+        <div>
           <span className="nav_button"><Link to="/Blog">Blog</Link></span>
           <span className="nav_button"><Link to="/Projects">Projects</Link></span>
-          <a className="gitHub_button"target="_blank" href="https://github.com/Gadget89">GitHub</a>
+          <a className="anchor_button"target="_blank" href="https://github.com/Gadget89">GitHub</a>
         </div>
         <p className="center_wrapper">
           <b>Nicolas Roybal</b>
@@ -20,11 +20,6 @@ export default React.createClass({
           <b>Bio</b>
           <br></br>
           I was born and raised in Northeastern New Mexico. Growing up in New Mexico I picked up a few hobbies including scoccer, skiing, and I gained my second degree black belt in Tae Kwon Do. In 2015 I relocated to San Antonio, TX to see what oppertunities a bigger city would have for me. I quickly found many jobs but ultimatly decided that I wanted to start on a new career path. Initally I was just going to "go back to school" but I found that was not time or cost effective for me. Luckily I stumbled upon a boot camp style front end engineering program at a school called The Iron Yard. I am currently attending the program and a little more than half way through it. Follow me through my journey here.
-        </p>
-        <p className="center_wrapper">
-          <b>GitHub</b>
-          <br></br>
-          <a target="_blank" href="https://github.com/Gadget89">Check out my work on GitHub</a>
         </p>
       </section>
     )
