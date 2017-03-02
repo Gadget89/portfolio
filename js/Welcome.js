e86683829cb5bb1b2233fe323bc769c47a01342c
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-03-02 00:32:13 -0600

    Changed nav buttons tohexagon shapes and made nav bar verticle on left side of page

--- js/Welcome.js
+++ js/Welcome.js
@@ -4,13 +4,25 @@ var FontAwesome = require('react-fontawesome');
 export default React.createClass({
   render() {
     return(
-      <section className="center_wrapper">
-        <h1 className="page_title">Welcome!</h1>
+      <main className="main_wrapper">
         <nav className="nav_bar">
-          <span className="nav_button"><Link to="/Blog">Blog <i className="fa fa-pencil-square-o"></i></Link></span>
-          <span className="nav_button"><Link to="/Projects">Projects <i className="fa fa-code"></i></Link></span>
-          <a className="anchor_button"target="_blank" href="https://github.com/Gadget89">GitHub <i className="fa fa-github"></i></a>
+          <div className="hex_top"></div>
+          <div className="hex_middle"><a target="_blank" href="https://github.com/Gadget89"><i className="fa fa-github fa-2x"></i></a></div>
+          <div className="hex_bottom"></div>
+
+          <div className="hex_top"></div>
+          <div className="hex_middle"><a target="_blank" href="https://medium.com/@nicolas.roybal/past-future-f982a66d5a66#.m9baco2be"><i className="fa fa-medium fa-2x"></i></a></div>
+          <div className="hex_bottom"></div>
+
+          <div className="hex_top"></div>
+          <div className="hex_middle"><span><Link to="/Blog"> <i className="fa fa-code fa-2x"></i></Link></span></div>
+          <div className="hex_bottom"></div>
+
+          <div className="hex_top"></div>
+          <div className="hex_middle"><a target="_blank" href="http://codepen.io/nickr999/"><i className="fa fa-codepen fa-2x"></i></a></div>
+          <div className="hex_bottom"></div>
         </nav>
+        <section className="center_wrapper">
         <img className="profile_img" src="styles/assets/me.jpg"></img>
         <p>
           <b>Nicolas Roybal</b>
@@ -25,6 +37,7 @@ export default React.createClass({
           I was born and raised in Northeastern New Mexico. Growing up in New Mexico I picked up a few hobbies including scoccer, skiing, and I gained my second degree black belt in Tae Kwon Do. In 2015 I relocated to San Antonio, TX to see what oppertunities a bigger city would have for me. I quickly found many jobs but ultimatly decided that I wanted to start on a new career path. Initally I was just going to "go back to school" but I found that was not time or cost effective for me. Luckily I stumbled upon a boot camp style front end engineering program at a school called The Iron Yard. I am currently attending the program and a little more than half way through it. Follow me through my journey here.
         </p>
       </section>
+      </main>
     )
   }
 })
