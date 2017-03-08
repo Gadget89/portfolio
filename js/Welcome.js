6fe9e6a82c8f8e8f6ca25009c1b70da12531eb6c
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-03-08 01:41:30 -0600

    Added a welcome section and added colors

--- js/Welcome.js
+++ js/Welcome.js
@@ -10,39 +10,46 @@ export default React.createClass({
     return(
       <main className="main_wrapper">
         <Header/>
+        <SideNav/>
         <section className="hex">
-          <div className="hex-row">
-            <div className="hex1">
+          <div className="welcome_hex">
             </div>
-            <div className="hex2">
-              <b className="title_letter1">N</b>
-              <b className="title_letter1-2">icolas</b>
+            <div className="hex1w">
             </div>
-            <div className="hex3">
+            <div className="hex2w">
+              <div className="hex_row">
+                <div className="hex1">
+                </div>
+                <div className="hex2">
+                  <b className="title_letter1">Nicolas</b>
+                  <b className="title_letter1-2"></b>
+                </div>
+                <div className="hex3">
+                </div>
+              </div>
+
+              <div className="new_hex">
+                <div className="hex1">
+                </div>
+                <div className="hex2">
+                  <b className="title_letter2">Roybal</b>
+                  <b className="title_letter2-2"></b>
+                </div>
+                <div className="hex3">
+                </div>
+            </div>
+            <div className="hex3w">
             </div>
           </div>
 
-          <div className="new_hex">
-            <div className="hex1">
-            </div>
-            <div className="hex2">
-              <b className="title_letter2">R</b>
-              <b className="title_letter2-2">oybal</b>
-            </div>
-            <div className="hex3">
-            </div>
-          </div>
-          <div>
-            <p className="sub_title">
-              <b>Portfolio</b>
-            </p>
-          </div>
         </section>
-        < SideNav/>
+
         <section className="center_wrapper">
+          <article className="shadow_box">
+            < About/>
+          </article>
         <section className="shadow_box">
           < Projects/>
-          < About/>
         </section>
         </section>
       </main>
